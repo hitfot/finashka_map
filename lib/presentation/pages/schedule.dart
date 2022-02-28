f3370160984870de1b3ce4c7ebd39d3921253212
Hitfot <hitfot@mail.ru>
2022-03-01 00:02:19 +0300

    Страница с расписанием

---
+++ lib/presentation/pages/schedule.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:finashka_map/presentation/colors.dart';
+
+class SchedulePage extends StatelessWidget{
+  const SchedulePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Расписание'),
+          backgroundColor: DarkThemeColors.background01
+      ),
+      backgroundColor: DarkThemeColors.background03,
+    );
+  }
+}
