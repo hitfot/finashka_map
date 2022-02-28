43d5e3cf5c80e4928ca5fbbb4cf6e001cb9f76be
Hitfot <hitfot@mail.ru>
2022-03-01 00:01:50 +0300

    Домашняя страница/стартовое окно

---
+++ lib/presentation/pages/Home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:finashka_map/presentation/colors.dart';
+import 'package:finashka_map/presentation/pages/map.dart';
+import 'package:finashka_map/presentation/pages/schedule.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Домашняя страница'),
+          centerTitle: true,
+          backgroundColor: DarkThemeColors.background01
+      ),
+      body: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: IconButton(
+                onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => const MapPage()));},
+                iconSize: 96.0,
+                icon: const Icon(Icons.apartment),
+                color: DarkThemeColors.secondary,
+              )
+            ),
+            Expanded(
+                child: IconButton(
+                  onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => const SchedulePage()));},
+                  iconSize: 96.0,
+                  icon: const Icon(Icons.article),
+                  color: DarkThemeColors.secondary,
+                )
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: DarkThemeColors.background03,
+    );
+  }
+}
+
