833e92ae1bf5e5738f0e824c1893fce576b53ca3
Hitfot <hitfot@mail.ru>
2022-03-01 00:02:38 +0300

    Главный файл

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,31 @@
+import 'package:finashka_map/presentation/pages/Home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:finashka_map/presentation/colors.dart';
+import 'package:finashka_map/presentation/pages/map.dart';
+import 'package:finashka_map/presentation/pages/schedule.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async{
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
 
+// class App extends StatelessWidget {
+//   const App({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context){
+//
+//     // Устанавливаем ориентацию телефона на вертикальную
+//     SystemChrome.setPreferredOrientations([
+//       DeviceOrientation.portraitDown,
+//       DeviceOrientation.portraitUp,
+//     ]);
+//
+//     // Сделали верхнюю шторку прозрачной
+//     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+//       statusBarColor: Colors.transparent,
+//     ));
+//   }
+// }
